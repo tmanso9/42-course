d5ecebc063396a5d526f15713dcd145ff8a41148
Teresa Outeiro Martins Manso <touteiro@c2r3s1.42lisboa.com>
2022-10-28 17:37:36 +0100

    libft - passed with 125%

---
+++ Finished_projects/libft/ft_putstr_fd.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: touteiro <touteiro@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/10 20:27:02 by touteiro          #+#    #+#             */
+/*   Updated: 2022/10/25 14:42:45 by touteiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+Outputs the string ’s’ to the given file descriptor.
+*/
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
