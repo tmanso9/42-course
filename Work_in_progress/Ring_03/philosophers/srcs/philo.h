dcb0ab923686611359dce3e2aeec6df577021dcc
Teresa Outeiro Martins Manso <touteiro@c1r7s3.42lisboa.com>
2023-01-20 20:41:12 +0000

    philosophers | started studying threads

---
+++ Work_in_progress/Ring_03/philosophers/srcs/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: touteiro <touteiro@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 16:05:46 by touteiro          #+#    #+#             */
+/*   Updated: 2023/01/20 20:23:28 by touteiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+
+
+#endif
