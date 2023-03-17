f23ad9c1a2accd559c1a0bec61269615c346e5bf
Teresa Outeiro Martins Manso <touteiro@c2r4s4.42lisboa.com>
2023-03-17 17:34:23 +0000

    chore: endline added

--- Work_in_progress/Ring_04/CPP/CPP00/ex00/megaphone.cpp
+++ Work_in_progress/Ring_04/CPP/CPP00/ex00/megaphone.cpp
@@ -3,7 +3,7 @@
 
 int	main(int argc, char **argv){
 	if (argc == 1)
-			std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; argv[i] ; i++)
 	{
 		for (int j = 0; argv[i][j] ; j++)
