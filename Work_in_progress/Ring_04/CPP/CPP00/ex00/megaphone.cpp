3ded21e1f2b646f00a9bd8352dead48d9320e092
tmanso9 <tmanso92@gmail.com>
2023-03-20 19:16:19 +0000

    feat: megaphone done

--- Work_in_progress/Ring_04/CPP/CPP00/ex00/megaphone.cpp
+++ Work_in_progress/Ring_04/CPP/CPP00/ex00/megaphone.cpp
@@ -4,11 +4,10 @@
 int	main(int argc, char **argv){
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	for (int i = 1; argv[i] ; i++)
+	for (int i = 1; i < argc ; i++)
 	{
 		for (int j = 0; argv[i][j] ; j++)
-			argv[i][j] = toupper(argv[i][j]);
-		std::cout << argv[i];
+			std::cout << (unsigned char)std::toupper(argv[i][j]);
 	}
 	std::cout << std::endl;
 }
