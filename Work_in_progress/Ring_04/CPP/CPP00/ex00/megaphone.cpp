7e26caef33703280bea8205512581e66c2c31c04
tmanso9 <tmanso92@gmail.com>
2023-03-22 18:31:18 +0000

    CPP00 ex00 | improved to use std::string

--- Work_in_progress/Ring_04/CPP/CPP00/ex00/megaphone.cpp
+++ Work_in_progress/Ring_04/CPP/CPP00/ex00/megaphone.cpp
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: touteiro <touteiro@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 18:27:24 by touteiro          #+#    #+#             */
+/*   Updated: 2023/03/22 18:27:38 by touteiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <iostream>
 
@@ -6,8 +17,9 @@ int	main(int argc, char **argv){
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < argc ; i++)
 	{
-		for (int j = 0; argv[i][j] ; j++)
-			std::cout << (unsigned char)std::toupper(argv[i][j]);
+		std::string	str = argv[i];
+		for (unsigned int j = 0; j < str.size(); j++)
+			std::cout << (unsigned char)std::toupper(*(str.begin() + j));
 	}
 	std::cout << std::endl;
 }
