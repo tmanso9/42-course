ae4a1bb8feacb268fb2919351ece93db144777ef
tmanso9 <tmanso92@gmail.com>
2023-03-25 16:47:35 +0000

    CPP01 | file structure improved

--- Work_in_progress/Ring_04/CPP/CPP01/ex01/src/main.cpp
+++ Work_in_progress/Ring_04/CPP/CPP01/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: touteiro <touteiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 17:31:02 by touteiro          #+#    #+#             */
-/*   Updated: 2023/03/23 18:03:54 by touteiro         ###   ########.fr       */
+/*   Updated: 2023/03/25 16:43:27 by touteiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ Zombie*	zombieHorde( int N, std::string name );
 int	main(void)
 {
 	int	N = 15;
-	Zombie	*horde = zombieHorde(N, "PghtEhriub");
+	Zombie	*horde = zombieHorde(N, "We are a horde");
 	for (int i = 0; i < N; i++)
 	{
 		std::cout << i << ": ";
