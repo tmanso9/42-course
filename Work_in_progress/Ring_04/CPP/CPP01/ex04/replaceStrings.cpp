1f30cb7ae7f908aed1935f582802486ac4f42c29
tmanso9 <tmanso92@gmail.com>
2023-03-25 12:36:46 +0000

    CPP01 ex04 | corrected and simplified code

--- Work_in_progress/Ring_04/CPP/CPP01/ex04/replaceStrings.cpp
+++ Work_in_progress/Ring_04/CPP/CPP01/ex04/replaceStrings.cpp
@@ -6,7 +6,7 @@
 /*   By: touteiro <touteiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 22:28:33 by touteiro          #+#    #+#             */
-/*   Updated: 2023/03/24 22:35:54 by touteiro         ###   ########.fr       */
+/*   Updated: 2023/03/25 12:32:12 by touteiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,24 @@
 
 void	replaceStrings(std::ifstream *infile, std::ofstream *outfile, std::string toFind, std::string toReplace)
 {
+	int linesRead = 0;
 	std::string	str;
 	while (std::getline(*infile, str))
 	{
-		int	i = str.find_first_of(*toFind.begin());
+		if (linesRead)
+			*outfile << std::endl;
+		linesRead++;
+
+		int	i = str.find(toFind);
 		while (i >= 0)
 		{
 			*outfile << str.substr(0, i);
 			str.erase(0, i);
-			if (!str.find(toFind))
-			{
-				*outfile << toReplace;
-				str.erase(0, toFind.size());
-			}
-			i = str.find_first_of(*toFind.begin(), 1);
+			*outfile << toReplace;
+			str.erase(0, toFind.size());
+			i = str.find(toFind);
 		}
-		*outfile << str << std::endl;
+
+		*outfile << str;
 	}
 }
