60e6365ae5972df6760359b5f994f9aa19fb90a9
tmanso9 <tmanso92@gmail.com>
2023-03-30 15:12:24 +0100

    CPP00 ex02 | started some stuff, gave up

--- Work_in_progress/Ring_04/CPP/CPP00/ex02/Account.cpp
+++ Work_in_progress/Ring_04/CPP/CPP00/ex02/Account.cpp
@@ -1,2 +1,50 @@
 
 #include "Account.hpp"
+#include <ctime>
+#include <iostream>
+#include <iomanip>
+
+Account::Account( int initial_deposit )
+{
+	this->_nbDeposits = 1;
+	this->_amount = initial_deposit;
+	this->_accountIndex = _nbAccounts;
+	_nbAccounts++;
+}
+
+
+Account::~Account( void )
+{
+}
+
+void	Account::_displayTimestamp( void )
+{
+	time_t	t = time(0);
+	tm	*tm = std::localtime(&t);
+
+	const char originalFill = std::cout.fill();
+	const int originalWidth = std::cout.width();
+
+	std::cout << "[" << 1900 + tm->tm_year;
+	std::cout << std::setfill('0') <<  std::setw(2) << (int)1 + tm->tm_mon;
+	
+	std::cout << tm->tm_mday;
+	std::cout << "_" << tm->tm_hour;
+	std::cout << tm->tm_min << tm->tm_sec << "]";
+	std::cout << std::setfill(originalFill) << std::setw(originalWidth);
+}
+
+void	Account::displayStatus (void) const {
+	std::cout << " index:" << this->_accountIndex << std::endl;
+}
+
+void	Account::displayAccountsInfos( void ){
+	Account::_displayTimestamp();
+
+	
+}
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
---
+++ Work_in_progress/Ring_04/CPP/CPP00/ex02/main.cpp
@@ -0,0 +1,10 @@
+#include "Account.hpp"
+
+int main()
+{
+	Account	a(100);
+	Account	b(100);
+
+	a.displayAccountsInfos();
+	b.displayAccountsInfos();
+}
