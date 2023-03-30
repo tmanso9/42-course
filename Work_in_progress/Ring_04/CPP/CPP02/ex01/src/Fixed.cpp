8bd4b62b80f1d94e1d7778b4e5139979eb9703e6
tmanso9 <tmanso92@gmail.com>
2023-03-30 18:04:12 +0100

    CPP02 ex01 | ex done

---
+++ Work_in_progress/Ring_04/CPP/CPP02/ex01/src/Fixed.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: touteiro <touteiro@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/30 14:02:56 by touteiro          #+#    #+#             */
+/*   Updated: 2023/03/30 18:02:33 by touteiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed( void )
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->_n = 0;
+}
+
+Fixed::~Fixed( void )
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed( Fixed const & src )
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+Fixed::Fixed( const int a )
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->setRawBits(a << _bits);
+}
+
+Fixed::Fixed( const float a )
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->setRawBits(round(a * (1 << _bits)));
+}
+
+Fixed &	Fixed::operator=(Fixed const & src)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->_n = src.getRawBits();
+	return (*this);
+}
+
+int	Fixed::getRawBits( void ) const
+{
+	return (this->_n);
+}
+
+void	Fixed::setRawBits (int const raw)
+{
+	this->_n = raw;
+}
+
+int		Fixed::toInt ( void ) const
+{
+	return this->getRawBits() >> _bits;
+}
+
+float		Fixed::toFloat(void) const
+{
+	return (float)this->getRawBits() / (1 << _bits) ;
+}
+
+std::ostream &	operator<<(std::ostream & out, Fixed const & num)
+{
+	out << num.toFloat();
+	return out;
+}
+
+const int Fixed::_bits = 8;
---
+++ Work_in_progress/Ring_04/CPP/CPP02/ex01/src/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: touteiro <touteiro@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/30 14:32:46 by touteiro          #+#    #+#             */
+/*   Updated: 2023/03/30 17:48:27 by touteiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main ( void )
+{
+	Fixed	a;
+	Fixed const	b( 10 );
+	Fixed const	c( 42.42f );
+	Fixed const d( b );
+
+	a = Fixed( 1234.4321f );
+
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+}
