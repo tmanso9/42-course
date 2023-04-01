0b5f00fb7f38790f12d21287f7b45762e57e8a58
tmanso9 <tmanso92@gmail.com>
2023-04-01 01:45:52 +0000

    CPP02 ex03 | main to be completed

--- Work_in_progress/Ring_04/CPP/CPP02/ex03/src/main.cpp
+++ Work_in_progress/Ring_04/CPP/CPP02/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: touteiro <touteiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 14:32:46 by touteiro          #+#    #+#             */
-/*   Updated: 2023/04/01 01:28:06 by touteiro         ###   ########.fr       */
+/*   Updated: 2023/04/01 01:44:30 by touteiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,37 @@ int	main ( void )
 	Point	a( -3.0f, -5.0f);
 	Point	b(7.0f, -3.0f);
 	Point	c ( 2.0f, 8.0f );
-	Point	Point ( 2.0f, -4.0f );
 
-	if (bsp(a, b, c, Point))
-		std::cout << "Point is inside triangle!" << std::endl;
-	else
-		std::cout << "No triangle for you!" << std::endl;
+	float	vals[10][2];
+	vals[0][0] = 2.0f;
+	vals[0][1] = -4.0f;
+	vals[1][0] = 1.0f;
+	vals[1][1] = -4.0f;
+	vals[2][0] = -2.0f;
+	vals[2][1] = -4.0f;
+	vals[3][0] = 20.0f;
+	vals[3][1] = -4.0f;
+
+	
+	Point	Tests[10];
+	
+	Tests[0] = Point( vals[0][0], vals[0][1] );
+	Tests[1] = Point( vals[1][0], vals[1][1] );
+	Tests[2] = Point( vals[2][0], vals[2][1] );
+	Tests[3] = Point( vals[3][0], vals[3][1] );
+	Tests[4] = Point( 2.0f, -4.0f );
+	Tests[5] = Point( 2.0f, -4.0f );
+	Tests[6] = Point( 2.0f, -4.0f );
+	Tests[7] = Point( 2.0f, -4.0f );
+	Tests[8] = Point( 2.0f, -4.0f );
+	Tests[9] = Point( 2.0f, -4.0f );
+
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << "Point (" << vals[i][0] << "," << vals[i][1] << ") "; 
+		if (bsp(a, b, c, Tests[i]))
+			std::cout << "is inside the triangle!" << std::endl;
+		else
+			std::cout << "is outside." << std::endl;
+	}
 }
