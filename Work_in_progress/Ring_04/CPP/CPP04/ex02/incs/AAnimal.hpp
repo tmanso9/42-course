f11f4d77ac7dedecf74905d9690e74e965c92811
tmanso9 <tmanso92@gmail.com>
2023-04-05 18:57:41 +0000

    CPP04 ex02 | updated AAnimal include guard

--- Work_in_progress/Ring_04/CPP/CPP04/ex02/incs/AAnimal.hpp
+++ Work_in_progress/Ring_04/CPP/CPP04/ex02/incs/AAnimal.hpp
@@ -6,13 +6,13 @@
 /*   By: touteiro <touteiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 10:56:33 by touteiro          #+#    #+#             */
-/*   Updated: 2023/04/05 12:46:36 by touteiro         ###   ########.fr       */
+/*   Updated: 2023/04/05 14:10:16 by touteiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-#ifndef ANIMAL_HPP
-# define ANIMAL_HPP
+#ifndef AANIMAL_HPP
+# define AANIMAL_HPP
 
 #include <iostream>
 #include <string>
