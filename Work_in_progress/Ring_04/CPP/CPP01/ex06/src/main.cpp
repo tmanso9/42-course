86839de0e94790fc0f7c30fe6013deb80c6aa2db
tmanso9 <tmanso92@gmail.com>
2023-04-11 12:27:40 +0100

    CPP01 ex06 | cleaned up code

--- Work_in_progress/Ring_04/CPP/CPP01/ex06/src/main.cpp
+++ Work_in_progress/Ring_04/CPP/CPP01/ex06/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: touteiro <touteiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 13:07:16 by touteiro          #+#    #+#             */
-/*   Updated: 2023/03/25 17:12:08 by touteiro         ###   ########.fr       */
+/*   Updated: 2023/04/10 20:41:03 by touteiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,18 @@
 
 int	main(int argc, char **argv)
 {
-	Harl		h1;
 	
 	if (argc == 2)
 	{
+		Harl		h1;
+		
 		std::cout << std::endl;
 		h1.complain(argv[1]);
 	}
 	else
-		std::cout << "Wrong number of arguments!" << std::endl;
+	{
+		std::cout << "Wrong number of arguments." << std::endl;
+		std::cout << "Usage: ./harlFilter <level>" << std::endl;
+	}
 	
 }
