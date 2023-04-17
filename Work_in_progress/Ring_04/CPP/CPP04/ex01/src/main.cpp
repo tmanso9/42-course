2df11b35764dbc421cd25e53891a36f33948919a
tmanso9 <tmanso92@gmail.com>
2023-04-17 14:46:04 +0100

    CPP04 ex01 | cleaned up main

--- Work_in_progress/Ring_04/CPP/CPP04/ex01/src/main.cpp
+++ Work_in_progress/Ring_04/CPP/CPP04/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: touteiro <touteiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 15:07:11 by touteiro          #+#    #+#             */
-/*   Updated: 2023/04/14 20:30:42 by touteiro         ###   ########.fr       */
+/*   Updated: 2023/04/17 14:45:04 by touteiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,28 @@ int	main(void)
 {
 	{
 		const	Animal*	bunch[6];
-	for (int i = 0; i < 6; i++)
-	{
-		if (i % 2)
-			bunch[i] = new Dog();
-		else
-			bunch[i] = new Cat();
-	}
-	for (int i = 0; i < 6; i++)
-	{
-		std::cout << i << ": ";
-		bunch[i]->makeSound();
-	}
-	for (int i = 0; i < 6; i++)
-		delete bunch[i];
+		for (int i = 0; i < 6; i++)
+		{
+			if (i % 2)
+				bunch[i] = new Dog();
+			else
+				bunch[i] = new Cat();
+		}
+		for (int i = 0; i < 6; i++)
+		{
+			std::cout << i << ": ";
+			bunch[i]->makeSound();
+		}
+		for (int i = 0; i < 6; i++)
+			delete bunch[i];
 	}
 	{
 		std::cout << std::endl << "Checking the copy constructors: " << std::endl;
-		const Dog	dog;
-		const	Animal* other = new Dog(dog);
+		const 	Dog*	dog = new Dog;
+		const	Animal* other = new Dog(*dog);
+		dog->makeSound();
 		other->makeSound();
+		delete dog;
 		delete other;
 	}
 }
